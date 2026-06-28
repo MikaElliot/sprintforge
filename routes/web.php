cc94b7802e1da54382119a26c3ca866c4af465a5
MikaElliot <mikaramiandrarivo@gmail.com>
2026-06-28 19:56:04 +0300

    refactor: reorganize routes web

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,5 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'show'])
 ->name('home.show');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
